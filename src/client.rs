2887a2beecf6417be5e901ce895f847fe79b2e7d
Yusuke Nojima <nojima@ynojima.com>
2025-08-07 16:46:15 +0900

    Add UploadRequest and DownloadRequest

--- src/client.rs
+++ src/client.rs
@@ -1,8 +1,10 @@
 use std::collections::HashMap;
 use std::fmt::Debug;
+use std::io::Read;
 
 use base64::Engine;
 use base64::engine::general_purpose::STANDARD_NO_PAD as BASE64;
+use rand::RngCore as _;
 use serde::Serialize;
 use serde::de::DeserializeOwned;
 
@@ -131,7 +133,14 @@ impl RequestBuilder {
     }
 
     pub fn call<Resp: DeserializeOwned>(self, client: &KintoneClient) -> ApiResult<Resp> {
-        let resp = self.make_request(client).call()?;
+        let resp = make_request(
+            client,
+            self.method,
+            &self.api_path,
+            self.headers,
+            self.query,
+        )
+        .call()?;
         Ok(resp.into_json()?)
     }
 
@@ -140,42 +149,143 @@ impl RequestBuilder {
         client: &KintoneClient,
         body: Body,
     ) -> ApiResult<Resp> {
-        let resp = self.make_request(client).send_json(body)?;
+        let resp = make_request(
+            client,
+            self.method,
+            &self.api_path,
+            self.headers,
+            self.query,
+        )
+        .send_json(body)?;
         Ok(resp.into_json()?)
     }
+}
 
-    fn make_request(mut self, client: &KintoneClient) -> ureq::Request {
-        match client.auth {
-            Auth::Password {
-                ref username,
-                ref password,
-            } => {
-                let body = format!("{username}:{password}");
-                let header_value = BASE64.encode(body);
-                self.headers
-                    .insert("x-cybozu-authorization".to_string(), header_value);
-            }
-            Auth::ApiToken { ref tokens } => {
-                self.headers
-                    .insert("x-cybozu-api-token".to_string(), tokens.join(","));
-            }
-        }
+pub struct UploadRequest {
+    method: http::Method,
+    api_path: String, // DO NOT include "/k" prefix
+    name: String,
+    filename: String,
+}
 
-        let mut u = client.base_url.clone();
-        if let Some(guest_space_id) = client.guest_space_id {
-            u = u.join(&format!("/k/guest/{guest_space_id}")).unwrap();
-        } else {
-            u = u.join("/k").unwrap();
+impl UploadRequest {
+    pub fn new(
+        method: http::Method,
+        api_path: impl Into<String>,
+        name: String,
+        filename: String,
+    ) -> Self {
+        Self {
+            method,
+            api_path: api_path.into(),
+            name,
+            filename,
         }
-        u = u.join(&self.api_path).unwrap();
-
-        let mut req = client.http_client.request(self.method.as_str(), u.as_str());
-        for (key, value) in self.query {
-            req = req.query(&key, &value);
-        }
-        for (key, value) in self.headers {
-            req = req.set(&key, &value);
-        }
-        req
     }
+
+    pub fn send(self, client: &KintoneClient, content: impl Read) -> ApiResult<ureq::Response> {
+        let mut rng = rand::rng();
+        let boundary = format!("{:#x}{:#x}", rng.next_u64(), rng.next_u64());
+
+        let content_type = format!("multipart/form-data; boundary={boundary}");
+        let mut headers = HashMap::with_capacity(1);
+        headers.insert("content-type".to_string(), content_type);
+
+        let req = make_request(client, self.method, &self.api_path, headers, HashMap::new());
+
+        let header = format!(
+            "--{boundary}\r\n\
+             content-disposition: form-data; name=\"{}\"; filename=\"{}\"\r\n\
+             \r\n",
+            self.name, self.filename
+        )
+        .into_bytes();
+        let footer = format!("\r\n--{boundary}--\r\n").into_bytes();
+        let body = header.chain(content).chain(&*footer);
+
+        let resp = req.send(body)?;
+        Ok(resp)
+    }
+}
+
+pub struct DownloadRequest {
+    method: http::Method,
+    api_path: String,               // DO NOT include "/k" prefix
+    query: HashMap<String, String>, // keys and values are NOT encoded
+}
+
+pub struct DownloadResponse {
+    pub mime_type: String,
+    pub content: Box<dyn Read + Send + Sync + 'static>,
+}
+
+impl DownloadRequest {
+    pub fn new(method: http::Method, api_path: impl Into<String>) -> Self {
+        Self {
+            method,
+            api_path: api_path.into(),
+            query: HashMap::new(),
+        }
+    }
+
+    pub fn query<V: Serialize>(mut self, key: &str, value: V) -> Self {
+        let value_str = serde_json::to_string(&value).unwrap();
+        self.query.insert(key.to_string(), value_str);
+        self
+    }
+
+    pub fn send(self, client: &KintoneClient) -> ApiResult<DownloadResponse> {
+        let req = make_request(
+            client,
+            self.method,
+            &self.api_path,
+            HashMap::new(),
+            self.query,
+        );
+        let resp = req.call()?;
+        let mime_type = resp.header("content-type").unwrap_or_default().to_owned();
+        Ok(DownloadResponse {
+            mime_type,
+            content: resp.into_reader(),
+        })
+    }
+}
+
+fn make_request(
+    client: &KintoneClient,
+    method: http::Method,
+    api_path: &str,
+    mut headers: HashMap<String, String>,
+    query: HashMap<String, String>,
+) -> ureq::Request {
+    match client.auth {
+        Auth::Password {
+            ref username,
+            ref password,
+        } => {
+            let body = format!("{username}:{password}");
+            let header_value = BASE64.encode(body);
+            headers.insert("x-cybozu-authorization".to_string(), header_value);
+        }
+        Auth::ApiToken { ref tokens } => {
+            headers.insert("x-cybozu-api-token".to_string(), tokens.join(","));
+        }
+    }
+
+    let mut u = client.base_url.clone();
+    if let Some(guest_space_id) = client.guest_space_id {
+        u = u.join(&format!("/k/guest/{guest_space_id}")).unwrap();
+    } else {
+        u = u.join("/k").unwrap();
+    }
+    u = u.join(api_path).unwrap();
+
+    let mut req = client.http_client.request(method.as_str(), u.as_str());
+    for (key, value) in query {
+        req = req.query(&key, &value);
+    }
+    for (key, value) in headers {
+        req = req.set(&key, &value);
+    }
+    req
 }
